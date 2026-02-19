d65a674cad1fae247184709bbc7d7a612d170f62
lorenamejiag <loregalvan0930@gmail.com>
2026-02-19 08:04:27 -0500

    Avance2

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,34 +1,40 @@
 package com.example;
 
+
 public class Main {
+
     public static void main(String[] args) {
-    Product product1 = new Product();
-    product1.setId("P001");
-    product1.setName("Laptop");
-    product1.setPrice(1500000);
-    product1.setStock(10);
 
-   Product product2 = new Product("P002", "Smartphone", 500000, 20);
-     
-   product1.setPrice(1400000);
-   product1.setStock(8);
+    
+        Product product1 = new Product();
+        product1.setId("P001");
+        product1.setName("Televisor");
+        product1.setPrice(1500000);
+        product1.setStock(40);
 
+    
+        Product product2 = new Product("002", "Nevera", 24000000, 100);
 
-    System.out.println("Product 1:");
-    System.out.println("ID: " + product1.getId());
-    System.out.println("Name: " + product1.getName());
-    System.out.println("Price: " + product1.getPrice());
-    System.out.println("Stock: " + product1.getStock());
+        
+        product2.setPrice(20.0);
+        product2.setStock(120);
 
-System.out.println("................................");
+    
+        System.out.println("Nombre producto 1: " + product1.getName());
+        System.out.println("Precio producto 1: " + product1.getPrice());
 
-    System.out.println("Product 2:");
-    System.out.println("ID: " + product2.getId());
-    System.out.println("Name: " + product2.getName());
-    System.out.println("Price: " + product2.getPrice());
-    System.out.println("Stock: " + product2.getStock());
-    }}
+        System.out.println("-------------------------");
 
+        System.out.println("Nombre producto 2: " + product2.getName());
+        System.out.println("Stock producto 2: " + product2.getStock());
+
+        System.out.println("-------------------------");
+
+        
+        System.out.println(product1.toString());
+        System.out.println(product2.toString());
+    }
+}
 
 
 
